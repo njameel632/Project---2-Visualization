cfc391e2cf16d62973794ef6a03156d1f1eadb9a
Luke <snekulekul@gmail.com>
2020-05-02 02:38:31 -0400

    we are fucked

--- SECOND TRY/static/js/logic.js
+++ SECOND TRY/static/js/logic.js
@@ -1,3 +1,46 @@
+var map = L.map("map").setView([35, -99], 4.3);
+
+// Add a tile layer
+L.tileLayer('https://api.maptiler.com/maps/positron/{z}/{x}/{y}.png?key=G18kR4B5cKkYaH1F1cW3',
+{attribution:'<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>'
+}
+).addTo(map)
+
+
+var data = clinicalTrials[2];
+
+for (var i = 0; i < data.length; ++i) {
+  var popupContent = 
+    '<br/><b>FACILITY:<b>   ' + data[i].facility +
+    '<br/><b>Brief Title:<b>' + data[i].brieftitle +
+    '<br/><b>Enrollment Count:</b> ' + data[i].enrollmentcount +
+    '<br/><b>Contact Name:</b> ' + data[i].contactname +
+    '<br/><b>Contact Phone:</b> ' + data[i].contactphone;
+
+    var popupOptions =
+    {
+      'maxWidth': '500',
+      'className': 'another-popup'
+    }
+  var m = L.marker([data[i].latitude, data[i].longitude], { icon: myIcon })
+    .bindPopup(popupContent, popupOptions);
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 console.log("hot")
 
 // listen for key responses
